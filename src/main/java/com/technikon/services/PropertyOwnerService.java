3d360eb187cba5c64fa04ce0f0699459884f0dff
johnbl <ic121012@di.uoa.gr>
2024-08-05 19:12:11 +0300

    property owner service, patterns and exceptions

--- src/main/java/com/technikon/services/PropertyOwnerService.java
+++ src/main/java/com/technikon/services/PropertyOwnerService.java
@@ -2,6 +2,25 @@
 package com.technikon.services;
 
 
+import com.technikon.model.PropertyOwner;
+
+import java.util.List;
+
 public interface PropertyOwnerService {
+
+    PropertyOwner get(Long id);
+
+    PropertyOwner create(String firstName,
+                         String lastName,
+                         String email,
+                         String userName,
+                         String phoneNumber,
+                         String address,
+                         String vat,
+                         String password);
+
+    void delete(Long id);
+
+    void update(PropertyOwner Owner);
     
 }
